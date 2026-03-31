3fd89cc92ad50db839aefc0798576446cf14214e
karlaraswanda <karla.raswanda@gmail.com>
2026-03-31 22:54:35 +0700

    Add authorization unit tests and fix environment config

--- biodata_kelompok/tests.py
+++ biodata_kelompok/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from allauth.socialaccount.models import SocialAccount
 
-# Create your tests here.
+from biodata_kelompok.views import is_authorized
+from biodata_kelompok.models import Biodata
+
+User = get_user_model()
+
+
+class AuthorizationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.allowed_email = settings.GROUP_MEMBER_EMAILS[0]
+
+        self.authorized_user = User.objects.create_user(
+            username='authorized',
+            email=self.allowed_email,
+            password='testpass123'
+        )
+
+        self.unauthorized_user = User.objects.create_user(
+            username='unauthorized',
+            email='notingroup@example.com',
+            password='testpass123'
+        )
+
+        SocialAccount.objects.create(
+            user=self.authorized_user,
+            provider='google',
+            uid='123'
+        )
+
+        SocialAccount.objects.create(
+            user=self.unauthorized_user,
+            provider='google',
+            uid='456'
+        )
+
+        Biodata.objects.create(user=self.authorized_user, bio='Authorized bio')
+        Biodata.objects.create(user=self.unauthorized_user, bio='Unauthorized bio')
+
+    def test_is_authorized_true(self):
+        self.assertTrue(is_authorized(self.authorized_user))
+
+    def test_is_authorized_false_not_in_group(self):
+        self.assertFalse(is_authorized(self.unauthorized_user))
+
+    def test_edit_denied_for_unauthorized_user(self):
+        self.client.login(username='unauthorized', password='testpass123')
+        response = self.client.get('/edit_biodata/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_allowed_for_authorized_user(self):
+        self.client.login(username='authorized', password='testpass123')
+        response = self.client.get('/edit_biodata/')
+        self.assertEqual(response.status_code, 200)
